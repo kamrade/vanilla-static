b438315165d14cb23884dfb66ed47c1eba757fc8
Denis Michailov <kamrad.reg@gmail.com>
2019-04-12 10:39:27 +1000

    Add real nice glitches

--- src/scripts/glitch.js
+++ src/scripts/glitch.js
@@ -1,5 +1,14 @@
 import $ from 'jquery';
 
 export default function glitch() {
+  const $btnAddClass = $('#btn-add-class');
+  const $title = $('.content__title');
+  const $indicator = $('#indicator');
 
+  $btnAddClass.on('click', () => {
+    $title.toggleClass('animated');
+    $title.hasClass('animated')
+      ? $btnAddClass.text('Animation: ON')
+      : $btnAddClass.text('Animation: OFF');
+  });
 }
