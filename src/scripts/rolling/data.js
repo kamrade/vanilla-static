a1e539743cca84acfed1f5ae8aa51a50581107e7
Denis Mikhailov <denis.michailov@gmail.com>
2019-07-23 17:21:06 +1000

    Add font Fixture. Add slides for mobile devices

--- src/scripts/rolling/data.js
+++ src/scripts/rolling/data.js
@@ -3,11 +3,11 @@
 //
 
 import slide01  from './animations/out/slide_01';
-import slide01V from './animations/out/slide_01V';
+// import slide01V from './animations/out/slide_01V';
 import slide02  from './animations/out/slide_02';
-import slide02V from './animations/out/slide_02V';
+// import slide02V from './animations/out/slide_02V';
 import slide03  from './animations/out/slide_03';
-import slide03V from './animations/out/slide_03V';
+// import slide03V from './animations/out/slide_03V';
 import slide04  from './animations/out/slide_09';
 
 export default {
@@ -15,17 +15,17 @@ export default {
     id: 1,
     element: 'slide_01_animation',
     animationData: slide01,
-    animationDataV: slide01V
+    animationDataV: slide01
   }, {
     id: 2,
     element: 'slide_02_animation',
     animationData: slide02,
-    animationDataV: slide02V
+    animationDataV: slide02
   }, {
     id: 3,
     element: 'slide_03_animation',
     animationData: slide03,
-    animationDataV: slide03V
+    animationDataV: slide03
   }, {
     id: 4,
     element: 'slide_04_animation',
